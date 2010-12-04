0b6844dce4bb78c9c1cf2cfe4c55c62a3dbe751e
c.horsdal <c.horsdal@27fd5a7c-5efa-8f4b-d402-5c82bed257ed>
2010-12-04 13:48:37 +0000

    Add NUnit and basic test project to solution

---
+++ UnicefVirtualWarehouse/UnicefVirtualWarehouseTest/DummyTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace UnicefVirtualWarehouseTest
+{
+    [TestFixture]
+    public class DummyTest
+    {
+        [Test]
+        public void AlwaysSucceed()
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}
