b9d2c05cb6d58a47fe5ac11a3198534c8a405b9b
c.horsdal <c.horsdal@27fd5a7c-5efa-8f4b-d402-5c82bed257ed>
2010-12-04 14:54:31 +0000

    Add basic domain model

---
+++ trunk/UnicefVirtualWarehouse/UnicefVirtualWarehouse/Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UnicefVirtualWarehouse.Models
+{
+    public class Contact
+    {
+        public int Id { get; private set; }
+        public string Address { get; set; }
+        public string Zip { get; set; }
+        public Country Country { get; set; }
+        public string City { get; set; }
+        public string Fax { get; set; }
+        public string Email { get; set; }
+        public Uri  Website { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ trunk/UnicefVirtualWarehouse/UnicefVirtualWarehouse/Models/Package.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UnicefVirtualWarehouse.Models
+{
+    public class Package
+    {
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public int MinUnit { get; set; }
+        public int Size { get; set; }
+        public int Price { get; set; }
+    }
+}
