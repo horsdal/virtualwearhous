e8be0396a5b5ef0c9e96e98be7160d4e4d60df5f
bdjukic@gmail.com <bdjukic@gmail.com@27fd5a7c-5efa-8f4b-d402-5c82bed257ed>
2010-12-04 19:55:22 +0000

    Adding new object model

--- trunk/UnicefVirtualWarehouse/UnicefVirtualWarehouse/Models/Manufacturer.cs
+++ trunk/UnicefVirtualWarehouse/UnicefVirtualWarehouse/Models/Manufacturer.cs
@@ -5,9 +5,11 @@ using System.Web;
 
 namespace UnicefVirtualWarehouse.Models
 {
-    public class Product
+    public class Manufacturer
     {
         public int Id { get; private set; }
         public string Name { get; set; }
+        public Contact Contact { get; set; }
+		public bool GMP { get; set; }
     }
 }
