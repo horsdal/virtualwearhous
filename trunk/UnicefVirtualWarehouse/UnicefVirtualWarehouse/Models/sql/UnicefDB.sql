8522db98166bdddc359d85a0e0eb4b516414cc6a
bdjukic@gmail.com <bdjukic@gmail.com@27fd5a7c-5efa-8f4b-d402-5c82bed257ed>
2010-12-04 15:54:12 +0000

    Fixed sql script, changed tabel creation order

--- trunk/UnicefVirtualWarehouse/UnicefVirtualWarehouse/Models/sql/UnicefDB.sql
+++ trunk/UnicefVirtualWarehouse/UnicefVirtualWarehouse/Models/sql/UnicefDB.sql
@@ -1,48 +1,4 @@
-﻿CREATE DATABASE [UnicefVirtualWarehouse] ON  PRIMARY 
-( NAME = N'UnicefVirtualWarehouse', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL10_50.MSSQLSERVER\MSSQL\DATA\UnicefVirtualWarehouse.mdf' , SIZE = 3072KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
- LOG ON 
-( NAME = N'UnicefVirtualWarehouse_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL10_50.MSSQLSERVER\MSSQL\DATA\UnicefVirtualWarehouse_log.ldf' , SIZE = 1024KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
-GO
-
-USE [UnicefVirtualWarehouse]
-GO
-
-/****** Object:  Table [dbo].[Consumer]    Script Date: 12/04/2010 16:13:18 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-SET ANSI_PADDING ON
-GO
-
-CREATE TABLE [dbo].[Consumer](
-	[ID] [int] IDENTITY(1,1) NOT NULL,
-	[Name] [varchar](200) NOT NULL,
-	[Contact_ID] [int] NOT NULL,
- CONSTRAINT [PK_Consumer] PRIMARY KEY CLUSTERED 
-(
-	[ID] ASC
-)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
-) ON [PRIMARY]
-
-GO
-
-SET ANSI_PADDING OFF
-GO
-
-ALTER TABLE [dbo].[Consumer]  WITH CHECK ADD  CONSTRAINT [FK_Consumer_Contact] FOREIGN KEY([Contact_ID])
-REFERENCES [dbo].[Contact] ([ID])
-GO
-
-ALTER TABLE [dbo].[Consumer] CHECK CONSTRAINT [FK_Consumer_Contact]
-GO
-
-USE [UnicefVirtualWarehouse]
-GO
-
-/****** Object:  Table [dbo].[Package]    Script Date: 12/04/2010 16:13:36 ******/
+﻿/****** Object:  Table [dbo].[Package]    Script Date: 12/04/2010 16:13:36 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -67,41 +23,6 @@ CREATE TABLE [dbo].[Package](
 GO
 
 SET ANSI_PADDING OFF
-GO
-
-USE [UnicefVirtualWarehouse]
-GO
-
-/****** Object:  Table [dbo].[Supplier]    Script Date: 12/04/2010 16:14:50 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-SET ANSI_PADDING ON
-GO
-
-CREATE TABLE [dbo].[Supplier](
-	[ID] [int] IDENTITY(1,1) NOT NULL,
-	[Name] [varchar](200) NOT NULL,
-	[Contact_ID] [int] NOT NULL,
- CONSTRAINT [PK_Supplier] PRIMARY KEY CLUSTERED 
-(
-	[ID] ASC
-)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
-) ON [PRIMARY]
-
-GO
-
-SET ANSI_PADDING OFF
-GO
-
-ALTER TABLE [dbo].[Supplier]  WITH CHECK ADD  CONSTRAINT [FK_Supplier_Contact] FOREIGN KEY([Contact_ID])
-REFERENCES [dbo].[Contact] ([ID])
-GO
-
-ALTER TABLE [dbo].[Supplier] CHECK CONSTRAINT [FK_Supplier_Contact]
 GO
 
 USE [UnicefVirtualWarehouse]
@@ -142,42 +63,6 @@ GO
 USE [UnicefVirtualWarehouse]
 GO
 
-/****** Object:  Table [dbo].[SupplierProduct]    Script Date: 12/04/2010 16:15:10 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[SupplierProduct](
-	[Supplier_ID] [int] NOT NULL,
-	[Product_ID] [int] NOT NULL,
-	[Licensed] [bit] NOT NULL,
- CONSTRAINT [PK_SupplierProduct] PRIMARY KEY CLUSTERED 
-(
-	[Supplier_ID] ASC,
-	[Product_ID] ASC
-)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
-) ON [PRIMARY]
-
-GO
-
-ALTER TABLE [dbo].[SupplierProduct]  WITH CHECK ADD  CONSTRAINT [FK_SupplierProduct_Product] FOREIGN KEY([Product_ID])
-REFERENCES [dbo].[Product] ([ID])
-GO
-
-ALTER TABLE [dbo].[SupplierProduct] CHECK CONSTRAINT [FK_SupplierProduct_Product]
-GO
-
-ALTER TABLE [dbo].[SupplierProduct]  WITH CHECK ADD  CONSTRAINT [FK_SupplierProduct_Supplier] FOREIGN KEY([Supplier_ID])
-REFERENCES [dbo].[Supplier] ([ID])
-GO
-
-ALTER TABLE [dbo].[SupplierProduct] CHECK CONSTRAINT [FK_SupplierProduct_Supplier]
-GO
-
-USE [UnicefVirtualWarehouse]
-GO
 
 /****** Object:  Table [dbo].[Country]    Script Date: 12/04/2010 16:15:16 ******/
 SET ANSI_NULLS ON
@@ -246,6 +131,77 @@ GO
 USE [UnicefVirtualWarehouse]
 GO
 
+/****** Object:  Table [dbo].[Consumer]    Script Date: 12/04/2010 16:13:18 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[Consumer](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](200) NOT NULL,
+	[Contact_ID] [int] NOT NULL,
+ CONSTRAINT [PK_Consumer] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+ALTER TABLE [dbo].[Consumer]  WITH CHECK ADD  CONSTRAINT [FK_Consumer_Contact] FOREIGN KEY([Contact_ID])
+REFERENCES [dbo].[Contact] ([ID])
+GO
+
+ALTER TABLE [dbo].[Consumer] CHECK CONSTRAINT [FK_Consumer_Contact]
+GO
+
+USE [UnicefVirtualWarehouse]
+GO
+
+/****** Object:  Table [dbo].[Supplier]    Script Date: 12/04/2010 16:14:50 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[Supplier](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](200) NOT NULL,
+	[Contact_ID] [int] NOT NULL,
+ CONSTRAINT [PK_Supplier] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+ALTER TABLE [dbo].[Supplier]  WITH CHECK ADD  CONSTRAINT [FK_Supplier_Contact] FOREIGN KEY([Contact_ID])
+REFERENCES [dbo].[Contact] ([ID])
+GO
+
+ALTER TABLE [dbo].[Supplier] CHECK CONSTRAINT [FK_Supplier_Contact]
+GO
+
+USE [UnicefVirtualWarehouse]
+GO
+
+
 /****** Object:  Table [dbo].[CountryOffer]    Script Date: 12/04/2010 16:15:29 ******/
 SET ANSI_NULLS ON
 GO
@@ -281,3 +237,40 @@ GO
 ALTER TABLE [dbo].[CountryOffer] CHECK CONSTRAINT [FK_CountryOffer_Package]
 GO
 
+
+/****** Object:  Table [dbo].[SupplierProduct]    Script Date: 12/04/2010 16:15:10 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[SupplierProduct](
+	[Supplier_ID] [int] NOT NULL,
+	[Product_ID] [int] NOT NULL,
+	[Licensed] [bit] NOT NULL,
+ CONSTRAINT [PK_SupplierProduct] PRIMARY KEY CLUSTERED 
+(
+	[Supplier_ID] ASC,
+	[Product_ID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[SupplierProduct]  WITH CHECK ADD  CONSTRAINT [FK_SupplierProduct_Product] FOREIGN KEY([Product_ID])
+REFERENCES [dbo].[Product] ([ID])
+GO
+
+ALTER TABLE [dbo].[SupplierProduct] CHECK CONSTRAINT [FK_SupplierProduct_Product]
+GO
+
+ALTER TABLE [dbo].[SupplierProduct]  WITH CHECK ADD  CONSTRAINT [FK_SupplierProduct_Supplier] FOREIGN KEY([Supplier_ID])
+REFERENCES [dbo].[Supplier] ([ID])
+GO
+
+ALTER TABLE [dbo].[SupplierProduct] CHECK CONSTRAINT [FK_SupplierProduct_Supplier]
+GO
+
+USE [UnicefVirtualWarehouse]
+GO
