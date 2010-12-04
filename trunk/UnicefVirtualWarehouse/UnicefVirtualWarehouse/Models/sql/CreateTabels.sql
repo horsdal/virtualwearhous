1ded1e38127aa14a4a36ba6432989a5714b6f43b
Bacher@gmail.com <Bacher@gmail.com@27fd5a7c-5efa-8f4b-d402-5c82bed257ed>
2010-12-04 17:20:10 +0000

    Moved Product-Package identifier from Product to Package

--- trunk/UnicefVirtualWarehouse/UnicefVirtualWarehouse/Models/sql/CreateTabels.sql
+++ trunk/UnicefVirtualWarehouse/UnicefVirtualWarehouse/Models/sql/CreateTabels.sql
@@ -8,7 +8,36 @@ DROP TABLE [dbo].[Product]
 DROP TABLE [dbo].[Package]
 GO
 
-/****** Object:  Table [dbo].[Package]    Script Date: 12/04/2010 16:13:36 ******/
+USE [UnicefVirtualWarehouse]
+GO
+
+/****** Object:  Table [dbo].[Product]    Script Date: 12/04/2010 18:17:18 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[Product](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](200) NOT NULL,
+ CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
+
+/****** Object:  Table [dbo].[Package]    Script Date: 12/04/2010 18:16:33 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -24,6 +53,7 @@ CREATE TABLE [dbo].[Package](
 	[MinUnit] [int] NOT NULL,
 	[Size] [int] NOT NULL,
 	[Price] [int] NOT NULL,
+	[Product_ID] [int] NOT NULL,
  CONSTRAINT [PK_Packages] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
@@ -34,45 +64,6 @@ GO
 
 SET ANSI_PADDING OFF
 GO
-
-USE [UnicefVirtualWarehouse]
-GO
-
-/****** Object:  Table [dbo].[Product]    Script Date: 12/04/2010 16:15:00 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-SET ANSI_PADDING ON
-GO
-
-CREATE TABLE [dbo].[Product](
-	[ID] [int] IDENTITY(1,1) NOT NULL,
-	[Name] [varchar](200) NOT NULL,
-	[Package_ID] [int] NOT NULL,
- CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED 
-(
-	[ID] ASC
-)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
-) ON [PRIMARY]
-
-GO
-
-SET ANSI_PADDING OFF
-GO
-
-ALTER TABLE [dbo].[Product]  WITH CHECK ADD  CONSTRAINT [FK_Product_Package] FOREIGN KEY([Package_ID])
-REFERENCES [dbo].[Package] ([ID])
-GO
-
-ALTER TABLE [dbo].[Product] CHECK CONSTRAINT [FK_Product_Package]
-GO
-
-USE [UnicefVirtualWarehouse]
-GO
-
 
 /****** Object:  Table [dbo].[Country]    Script Date: 12/04/2010 16:15:16 ******/
 SET ANSI_NULLS ON
