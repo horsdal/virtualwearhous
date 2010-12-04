01579ae8970e1162211fdc2cd63d68b49df7ce3a
Bacher@gmail.com <Bacher@gmail.com@27fd5a7c-5efa-8f4b-d402-5c82bed257ed>
2010-12-04 21:00:10 +0000

    Added ID property

--- trunk/UnicefVirtualWarehouse/UnicefVirtualWarehouse/Models/ManufacturerPresentation.cs
+++ trunk/UnicefVirtualWarehouse/UnicefVirtualWarehouse/Models/ManufacturerPresentation.cs
@@ -7,6 +7,7 @@ namespace UnicefVirtualWarehouse.Models
 {
     public class ManufacturerPresentation
     {
+        public int ID { get; set; }
 		public Manufacturer Manufacturer { get; private set; }
 		public Presentation Presentation { get; set; }
 		public Contact ManufacturingSite { get; set; }
