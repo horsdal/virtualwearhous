6f1a304c9930cbc9692e9579d300ceeea6ccdb40
c.horsdal <c.horsdal@27fd5a7c-5efa-8f4b-d402-5c82bed257ed>
2010-12-04 14:54:31 +0000

    Add basic domain model

---
+++ UnicefVirtualWarehouse/UnicefVirtualWarehouse/Models/Supplier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UnicefVirtualWarehouse.Models
+{
+    public class Supplier
+    {
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public Contact Contact { get; set; }
+    }
+}
