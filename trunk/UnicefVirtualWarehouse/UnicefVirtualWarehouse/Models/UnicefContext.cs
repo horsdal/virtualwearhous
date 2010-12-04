aa942e1f0b0349ec8a11aded54a9c0decd131ce3
c.horsdal <c.horsdal@27fd5a7c-5efa-8f4b-d402-5c82bed257ed>
2010-12-04 20:37:55 +0000

    Adding EF4

---
+++ trunk/UnicefVirtualWarehouse/UnicefVirtualWarehouse/Models/UnicefContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using UnicefVirtualWarehouse.Models;
+
+
+namespace UnicefVirtualWarehouse
+{
+    public class UnicefContext : DbContext
+    {
+        public DbSet<Consumer> Consumers { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+        public DbSet<ManufacturerPresentation> ManufacturerPresentations { get; set; }
+        public DbSet<Presentation> Presentations { get; set; }
+    }
+}
