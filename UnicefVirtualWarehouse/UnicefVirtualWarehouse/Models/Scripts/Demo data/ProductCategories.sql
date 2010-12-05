4ca9d5939c604bb9ab67358465690f1f695500bc
bdjukic@gmail.com <bdjukic@gmail.com@27fd5a7c-5efa-8f4b-d402-5c82bed257ed>
2010-12-05 10:41:47 +0000

    Adding demo scripts

---
+++ UnicefVirtualWarehouse/UnicefVirtualWarehouse/Models/Scripts/Demo data/ProductCategories.sql
@@ -0,0 +1,65 @@
+INSERT INTO [UnicefVirtualWarehouse.UnicefContext].[dbo].[ProductCategories]
+           ([Name])
+     VALUES
+           ('DIARRHOEA Fluid and electrolyte replacement')
+GO
+
+INSERT INTO [UnicefVirtualWarehouse.UnicefContext].[dbo].[ProductCategories]
+           ([Name])
+     VALUES
+           ('HIV/AIDS')
+GO
+
+INSERT INTO [UnicefVirtualWarehouse.UnicefContext].[dbo].[ProductCategories]
+           ([Name])
+     VALUES
+           ('MALARIA')
+GO
+
+INSERT INTO [UnicefVirtualWarehouse.UnicefContext].[dbo].[ProductCategories]
+           ([Name])
+     VALUES
+           ('MATERNAL AND NEWBORN CARE')
+GO
+
+INSERT INTO [UnicefVirtualWarehouse.UnicefContext].[dbo].[ProductCategories]
+           ([Name])
+     VALUES
+           ('NEGLECTED TROPICAL DISEASES')
+GO
+
+INSERT INTO [UnicefVirtualWarehouse.UnicefContext].[dbo].[ProductCategories]
+           ([Name])
+     VALUES
+           ('NUTRITION Vitamin and mineral supplementation')
+GO
+
+INSERT INTO [UnicefVirtualWarehouse.UnicefContext].[dbo].[ProductCategories]
+           ([Name])
+     VALUES
+           ('OPPORTUNISTIC INFECTIONS')
+GO
+
+INSERT INTO [UnicefVirtualWarehouse.UnicefContext].[dbo].[ProductCategories]
+           ([Name])
+     VALUES
+           ('PAIN AND PALLIATIVE CARE')
+GO
+
+INSERT INTO [UnicefVirtualWarehouse.UnicefContext].[dbo].[ProductCategories]
+           ([Name])
+     VALUES
+           ('PNEUMONIA')
+GO
+
+INSERT INTO [UnicefVirtualWarehouse.UnicefContext].[dbo].[ProductCategories]
+           ([Name])
+     VALUES
+           ('TUBERCULOSIS')
+GO
+
+INSERT INTO [UnicefVirtualWarehouse.UnicefContext].[dbo].[ProductCategories]
+           ([Name])
+     VALUES
+           ('OTHER CAUSES')
+GO
