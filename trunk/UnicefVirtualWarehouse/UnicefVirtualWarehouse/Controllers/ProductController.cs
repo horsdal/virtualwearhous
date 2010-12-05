a32a4aea21d7e56e3a11ef504186247f01231032
c.horsdal <c.horsdal@27fd5a7c-5efa-8f4b-d402-5c82bed257ed>
2010-12-05 01:16:38 +0000

    Add product creation page.

--- trunk/UnicefVirtualWarehouse/UnicefVirtualWarehouse/Controllers/ProductController.cs
+++ trunk/UnicefVirtualWarehouse/UnicefVirtualWarehouse/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using UnicefVirtualWarehouse.Models;
+using System.Data.Entity;
 
 namespace UnicefVirtualWarehouse.Controllers
 {
@@ -24,6 +25,22 @@ namespace UnicefVirtualWarehouse.Controllers
             var productCategory = MvcApplication.CurrentUnicefContext.ProductCatagories.Include("Products").SingleOrDefault(p => p.Id == id);
 
             return View("index", productCategory.Products.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Create(FormCollection form)
+        {
+            var product = new Product { Name = form["Name"], Presentations = new List<Presentation>() };
+            var db = MvcApplication.CurrentUnicefContext;
+            db.Product.Add(product);
+            db.SaveChanges();
+            
+            return RedirectToAction("Index");
         }
     }
 }
