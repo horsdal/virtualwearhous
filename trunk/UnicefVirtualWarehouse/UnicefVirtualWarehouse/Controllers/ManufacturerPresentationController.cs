9216287e26e31669a2f8f5d1fcafcfb254c62b16
bdjukic@gmail.com <bdjukic@gmail.com@27fd5a7c-5efa-8f4b-d402-5c82bed257ed>
2010-12-05 00:18:10 +0000

    adding man present view

---
+++ trunk/UnicefVirtualWarehouse/UnicefVirtualWarehouse/Controllers/ManufacturerPresentationController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UnicefVirtualWarehouse.Models;
+
+namespace UnicefVirtualWarehouse.Controllers
+{
+    public class ManufacturerPresentationController : Controller
+    {
+        //
+        // GET: /ManufacturerPresentation/
+
+        public ActionResult Index()
+        {
+			return View();
+        }
+
+        //
+        // GET: /ManufacturerPresentation/Details/5
+
+		public ActionResult Details(int id)
+        {
+			IList<ManufacturerPresentation> manPresentations =
+				MvcApplication.CurrentUnicefContext.ManufacturerPresentations.Include("Presentation").Include("Manufacturer").Where(manPres => manPres.Presentation.Id == id).ToList();
+
+			return View("Index", manPresentations);            
+        }
+
+        //
+        // GET: /ManufacturerPresentation/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /ManufacturerPresentation/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        //
+        // GET: /ManufacturerPresentation/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /ManufacturerPresentation/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /ManufacturerPresentation/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /ManufacturerPresentation/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- trunk/UnicefVirtualWarehouse/UnicefVirtualWarehouse/Views/Presentation/Index.aspx
+++ trunk/UnicefVirtualWarehouse/UnicefVirtualWarehouse/Views/Presentation/Index.aspx
@@ -1,10 +1,17 @@
-<%@ Page Title="" Language="C#" Inherits="System.Web.Mvc.ViewPage<System.Collections.Generic.List<UnicefVirtualWarehouse.Models.Presentation>>" MasterPageFile="~/Views/Shared/Site.Master" %>
+﻿<%@ Page Title="" Language="C#" Inherits="System.Web.Mvc.ViewPage<System.Collections.Generic.IList<UnicefVirtualWarehouse.Models.Presentation>>" MasterPageFile="~/Views/Shared/Site.Master" %>
 <asp:Content runat="server" ID="Title" ContentPlaceHolderID="TitleContent"></asp:Content>
 <asp:Content runat="server" ID="Main" ContentPlaceHolderID="MainContent">
 <% if(Model.Any()) { %>
-    <% foreach(var p in Model){%>
-    <a href=/Presentation/Details/<%=p.Id %>><%=p.Name %></a><br />
-    <%}%>
+	<table>
+		<th> Presentation </th>
+			<% foreach(var p in Model){%>
+			<tr>
+				<td>
+					<a  href="../ManfucaturerPresentation/"><%=p.Name%></a>
+				</td>
+			</tr>
+			<%}%>
+	</table> 
 <%}else{%>
     <p>No presentations available</p>
 <%}%>
