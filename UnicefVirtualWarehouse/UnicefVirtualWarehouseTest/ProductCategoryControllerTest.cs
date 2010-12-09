80a0eabc371a59e32854a60d0de402f6e942b087
c.horsdal <c.horsdal@27fd5a7c-5efa-8f4b-d402-5c82bed257ed>
2010-12-09 21:31:22 +0000

    Productcategory creation works. And is unit tested.

---
+++ UnicefVirtualWarehouse/UnicefVirtualWarehouseTest/ProductCategoryControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using NUnit.Framework;
+using UnicefVirtualWarehouse.Controllers;
+using UnicefVirtualWarehouse.Models.Repositories;
+
+namespace UnicefVirtualWarehouseTest
+{
+    public class ProductCategoryControllerTest : ControllerTestBase<ProductCategoryController>
+    {
+        [Test]
+        public void CanAddAndFindAProduct()
+        {
+            var newProdcutCategoryName = string.Format("New test product category #{0}#", DateTime.Now.Ticks);
+
+            var repo = new ProductCategoryRepository();
+
+            controllerUnderTest.Create(new FormCollection(new NameValueCollection { { "Name", newProdcutCategoryName } }));
+
+            var categories = repo.GetByName(newProdcutCategoryName);
+            Assert.That(categories.Count, Is.EqualTo(1));
+            Assert.That(categories[0].Name, Is.EqualTo(newProdcutCategoryName));
+        }
+    }
+}
