f4c98ec347e88f19abd3b162894688b45607a1f3
c.horsdal <c.horsdal@27fd5a7c-5efa-8f4b-d402-5c82bed257ed>
2010-12-09 21:02:15 +0000

    Add a repository for products, a test for product controller and some test infrastructutre.

---
+++ UnicefVirtualWarehouse/UnicefVirtualWarehouseTest/FakeApp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnicefVirtualWarehouse;
+
+namespace UnicefVirtualWarehouseTest
+{
+    public class FakeApp : MvcApplication
+    {
+        public FakeApp()
+        {
+            this.Init();
+        }
+
+        public void BeginTest()
+        {
+            Application_BeginRequest(this, new EventArgs());
+        }
+
+        public void EndTest()
+        {
+            Application_EndRequest(this, new EventArgs());
+        }
+    }
+}
---
+++ UnicefVirtualWarehouse/UnicefVirtualWarehouseTest/ProductControllerTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using NUnit.Framework;
+using Rhino.Mocks;
+using UnicefVirtualWarehouse.Controllers;
+using UnicefVirtualWarehouse.Models;
+using UnicefVirtualWarehouse.Models.Repositories;
+
+namespace UnicefVirtualWarehouseTest
+{
+    [TestFixture]
+    public class ProductControllerTest
+    {
+        private FakeApp app;
+        private MockRepository mocks;
+        private HttpContextBase mockedhttpContext;
+        private HttpRequestBase mockedHttpRequest;
+        private ProductController controllerUnderTest;
+
+        [TestFixtureSetUp]
+        public void FixtureSetup()
+        {
+            app = new FakeApp();
+
+            mocks = new MockRepository();
+            mockedhttpContext = mocks.DynamicMock<HttpContextBase>();
+            mockedHttpRequest = mocks.DynamicMock<HttpRequestBase>();
+            SetupResult.For(mockedhttpContext.Request).Return(mockedHttpRequest);
+            SetupResult.For(mockedHttpRequest.IsAuthenticated).Return(true); // acts as if locked in
+
+            mocks.ReplayAll();
+        }
+
+        [SetUp]
+        public void TestSetup()
+        {
+            app.BeginTest();
+
+            controllerUnderTest = new ProductController();
+            controllerUnderTest.ControllerContext = new ControllerContext(mockedhttpContext, new RouteData(), controllerUnderTest);
+        }
+
+        [TearDown]
+        public void TestTeardown()
+        {
+            app.EndTest();
+        }
+
+        [Test]
+        public void CanAddAndFindAProduct()
+        {
+            var newProductName = string.Format("New test product #{0}#", DateTime.Now.Ticks);
+
+            var repo = new ProductRepository();
+            var catogoryId = 1; 
+
+            controllerUnderTest.Create(new FormCollection(new NameValueCollection {{"Key.Name", newProductName}, {"Value", catogoryId.ToString()}}));
+
+            var products = repo.GetByName(newProductName);
+            Assert.That(products.Count, Is.EqualTo(catogoryId));
+            Assert.That(products[0].Name, Is.EqualTo(newProductName));
+        }
+    }
+}
