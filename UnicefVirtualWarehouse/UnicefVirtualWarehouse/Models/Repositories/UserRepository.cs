ffcce4120499d883dec07de10b85b1b1d8f998fe
c.horsdal <c.horsdal@27fd5a7c-5efa-8f4b-d402-5c82bed257ed>
2010-12-21 21:22:14 +0000

    Adding users to model. These should match the user in the form authentication store, and provide the appropriate roles to users. These two are not connected yet.

---
+++ UnicefVirtualWarehouse/UnicefVirtualWarehouse/Models/Repositories/UserRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace UnicefVirtualWarehouse.Models.Repositories
+{
+    public class UserRepository
+    {
+        private UnicefContext db;
+
+        public UserRepository()
+        {
+            db = MvcApplication.CurrentUnicefContext;
+        }
+
+        public bool Add(User newUser)
+        {
+            if (db.Users.FirstOrDefault(u => u.UserName == newUser.UserName) != null)
+                return false;
+
+            db.Users.Add(newUser);
+            return db.SaveChanges() > 0;
+        }
+
+        public User GetByName(string userName)
+        {
+            return db.Users.FirstOrDefault(u => u.UserName == userName);
+        }
+
+        public bool Delete(User user)
+        {
+            db.Users.Remove(user);
+            return db.SaveChanges() > 0;
+        }
+    }
+}
