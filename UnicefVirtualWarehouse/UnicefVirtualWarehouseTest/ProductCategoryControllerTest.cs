ea689d0119d00d7414b0c51762838a6b15152a8b
c.horsdal <c.horsdal@27fd5a7c-5efa-8f4b-d402-5c82bed257ed>
2011-01-05 23:52:12 +0000

    ProductCategory creation only allowed for unicef and admin users

--- UnicefVirtualWarehouse/UnicefVirtualWarehouseTest/ProductCategoryControllerTest.cs
+++ UnicefVirtualWarehouse/UnicefVirtualWarehouseTest/ProductCategoryControllerTest.cs
@@ -5,15 +5,21 @@ using System.Text;
 using System.Web.Mvc;
 using NUnit.Framework;
 using UnicefVirtualWarehouse.Controllers;
+using UnicefVirtualWarehouse.Models;
 using UnicefVirtualWarehouse.Models.Repositories;
 
 namespace UnicefVirtualWarehouseTest
 {
-    public class ProductCategoryControllerTest : ControllerTestBase<ProductCategoryController>
+    public class ProductCategoryControllerTestLoggedInAsUnicef : ControllerTestBase<ProductCategoryController>
     {
         protected override bool IsLoggedIn()
         {
             return true;
+        }
+
+        protected override string Role()
+        {
+            return UnicefRole.Unicef.ToString();
         }
 
         [Test]
@@ -29,5 +35,87 @@ namespace UnicefVirtualWarehouseTest
             Assert.That(categories.Count, Is.EqualTo(1));
             Assert.That(categories[0].Name, Is.EqualTo(newProdcutCategoryName));
         }
+
+        [Test]
+        public void CanAccessCreatePage()
+        {
+            var view = controllerUnderTest.Create() as ViewResult;
+            Assert.That(view, Is.Not.Null);
+            Assert.That(view.ViewName, Is.EqualTo("")); //that is the default view for the action
+        }
     }
+
+    public class ProductCategoryControllerTestLoggedInAsAdmin : ControllerTestBase<ProductCategoryController>
+    {
+        protected override bool IsLoggedIn()
+        {
+            return true;
+        }
+
+        protected override string Role()
+        {
+            return UnicefRole.Administrator.ToString();
+        }
+
+        [Test]
+        public void CanAddAndFindAProductCategory()
+        {
+            var newProdcutCategoryName = string.Format("New test product category #{0}#", DateTime.Now.Ticks);
+
+            var repo = new ProductCategoryRepository();
+
+            controllerUnderTest.Create(new FormCollection(new NameValueCollection { { "Name", newProdcutCategoryName } }));
+
+            var categories = repo.GetByName(newProdcutCategoryName);
+            Assert.That(categories.Count, Is.EqualTo(1));
+            Assert.That(categories[0].Name, Is.EqualTo(newProdcutCategoryName));
+        }
+
+        [Test]
+        public void CanAccessCreatePage()
+        {
+            var view = controllerUnderTest.Create() as ViewResult;
+            Assert.That(view, Is.Not.Null);
+            Assert.That(view.ViewName, Is.EqualTo("")); //that is the default view for the action
+        }
+    }
+
+    [TestFixture]
+    class ProductCategoryControllerTestAsNotLoggedIn : ControllerTestBase<ProductCategoryController>
+    {
+        [Test]
+        public void CreateRedirectsToIndex()
+        {
+            var res = controllerUnderTest.Create() as RedirectToRouteResult;
+            Assert.That(res.RouteValues.Values, Contains.Item("Index"));
+
+            res = controllerUnderTest.Create(new FormCollection()) as RedirectToRouteResult;
+            Assert.That(res.RouteValues.Values, Contains.Item("Index"));
+        }
+    }
+
+    [TestFixture]
+    class ProductCategoryControllerTestLoggedInAsManufacturer : ControllerTestBase<ProductCategoryController>
+    {
+        protected override bool IsLoggedIn()
+        {
+            return true;
+        }
+
+        protected override string Role()
+        {
+            return UnicefRole.Manufacturer.ToString();
+        }
+
+        [Test]
+        public void CreateRedirectsToIndex()
+        {
+            var res = controllerUnderTest.Create() as RedirectToRouteResult;
+            Assert.That(res.RouteValues.Values, Contains.Item("Index"));
+
+            res = controllerUnderTest.Create(new FormCollection()) as RedirectToRouteResult;
+            Assert.That(res.RouteValues.Values, Contains.Item("Index"));
+        }
+    }
+
 }
