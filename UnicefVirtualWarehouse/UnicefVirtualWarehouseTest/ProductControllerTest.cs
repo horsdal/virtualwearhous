cd4afb290e2025586c74f41705116f1284120f5c
c.horsdal <c.horsdal@27fd5a7c-5efa-8f4b-d402-5c82bed257ed>
2011-01-05 23:58:21 +0000

    Products can only be added by unicef and admin users.

--- UnicefVirtualWarehouse/UnicefVirtualWarehouseTest/ProductControllerTest.cs
+++ UnicefVirtualWarehouse/UnicefVirtualWarehouseTest/ProductControllerTest.cs
@@ -13,12 +13,26 @@ using UnicefVirtualWarehouse.Models.Repositories;
 namespace UnicefVirtualWarehouseTest
 {
     [TestFixture]
-    public class ProductControllerTest : ControllerTestBase<ProductController>
+    public class ProductControllerTestLoggedInAsUnicef : ControllerTestBase<ProductController>
     {
         protected override bool IsLoggedIn()
         {
             return true;
         }
+
+        protected override string Role()
+        {
+            return UnicefRole.Unicef.ToString();
+        }
+
+        [Test]
+        public void CanAccessCreatePage()
+        {
+            var view = controllerUnderTest.Create() as ViewResult;
+            Assert.That(view, Is.Not.Null);
+            Assert.That(view.ViewName, Is.EqualTo("")); //that is the default view for the action
+        }
+
         [Test]
         public void CanAddAndFindAProduct()
         {
@@ -34,4 +48,81 @@ namespace UnicefVirtualWarehouseTest
             Assert.That(products[0].Name, Is.EqualTo(newProductName));
         }
     }
+
+      [TestFixture]
+    public class ProductControllerTestLoggedInAsAdmin : ControllerTestBase<ProductController>
+    {
+        protected override bool IsLoggedIn()
+        {
+            return true;
+        }
+
+        protected override string Role()
+        {
+            return UnicefRole.Administrator.ToString();
+        }
+
+        [Test]
+        public void CanAccessCreatePage()
+        {
+            var view = controllerUnderTest.Create() as ViewResult;
+            Assert.That(view, Is.Not.Null);
+            Assert.That(view.ViewName, Is.EqualTo("")); //that is the default view for the action
+        }
+
+        [Test]
+        public void CanAddAndFindAProduct()
+        {
+            var newProductName = string.Format("New test product #{0}#", DateTime.Now.Ticks);
+
+            var repo = new ProductRepository();
+            var catogoryId = 1; 
+
+            controllerUnderTest.Create(new FormCollection(new NameValueCollection {{"Key.Name", newProductName}, {"Value", catogoryId.ToString()}}));
+
+            var products = repo.GetByName(newProductName);
+            Assert.That(products.Count, Is.EqualTo(1));
+            Assert.That(products[0].Name, Is.EqualTo(newProductName));
+        }
+    }
+
+      [TestFixture]
+      class ProductControllerTestAsNotLoggedIn : ControllerTestBase<ProductController>
+      {
+          [Test]
+          public void CreateRedirectsToIndex()
+          {
+              var res = controllerUnderTest.Create() as RedirectToRouteResult;
+              Assert.That(res.RouteValues.Values, Contains.Item("Index"));
+
+              res = controllerUnderTest.Create(new FormCollection()) as RedirectToRouteResult;
+              Assert.That(res.RouteValues.Values, Contains.Item("Index"));
+          }
+      }
+
+      [TestFixture]
+      class ProductControllerTestLoggedInAsManufacturer : ControllerTestBase<ProductController>
+      {
+          protected override bool IsLoggedIn()
+          {
+              return true;
+          }
+
+          protected override string Role()
+          {
+              return UnicefRole.Manufacturer.ToString();
+          }
+
+          [Test]
+          public void CreateRedirectsToIndex()
+          {
+              var res = controllerUnderTest.Create() as RedirectToRouteResult;
+              Assert.That(res.RouteValues.Values, Contains.Item("Index"));
+
+              res = controllerUnderTest.Create(new FormCollection()) as RedirectToRouteResult;
+              Assert.That(res.RouteValues.Values, Contains.Item("Index"));
+          }
+      }
+
+
 }
