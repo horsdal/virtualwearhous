2c645e181109013158cd71b5db4e1ef839f12031
c.horsdal <c.horsdal@27fd5a7c-5efa-8f4b-d402-5c82bed257ed>
2011-04-07 08:33:27 +0000

    Remove some dead code from repositories.

--- trunk/UnicefVirtualWarehouse/UnicefVirtualWarehouse/Models/Repositories/ManufacturerPresentationRepository.cs
+++ trunk/UnicefVirtualWarehouse/UnicefVirtualWarehouse/Models/Repositories/ManufacturerPresentationRepository.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
 
 namespace UnicefVirtualWarehouse.Models.Repositories
 {
